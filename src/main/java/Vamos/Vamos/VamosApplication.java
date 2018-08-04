0bd7e4c552b3179d38cd68d176f92c704d2fb190
dhaarm11 <37424669+dhaarm11@users.noreply.github.com>
2018-08-04 15:26:12 -0500

    Add files via upload
    Project Setup

---
+++ src/main/java/Vamos/Vamos/VamosApplication.java
@@ -0,0 +1,12 @@
+package Vamos.Vamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VamosApplication.class, args);
+	}
+}
---
+++ src/main/java/Vamos/Vamos/controllers/DetailsController.java
@@ -0,0 +1,17 @@
+package Vamos.Vamos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({""})
+public class DetailsController {
+    public DetailsController() {
+    }
+
+    @RequestMapping({"search"})
+    public String index(Model model) {
+        return "search";
+    }
+}
