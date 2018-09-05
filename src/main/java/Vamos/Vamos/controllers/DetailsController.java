b86e2a74e011db273fbd04f716dd068631681781
David Haarmann <david.haarmann@gmail.com>
2018-09-04 19:05:47 -0500

    Added Filter By Category

--- src/main/java/Vamos/Vamos/controllers/DetailsController.java
+++ src/main/java/Vamos/Vamos/controllers/DetailsController.java
@@ -1,10 +1,15 @@
 package Vamos.Vamos.controllers;
 
+import Vamos.Vamos.models.Location;
 import Vamos.Vamos.models.data.LocationDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping({""})
@@ -20,4 +25,13 @@ public class DetailsController {
 
             return "search";
         }
+
+    @RequestMapping(value = "search/{city}", method = RequestMethod.GET)
+    public String getLocationsByCity(@PathVariable("city") String city, Model model) {
+        List<Location> locations = locationDao.findAllByCity(city);
+        model.addAttribute("locations", locations);
+        model.addAttribute("title", "Search");
+        return "search";
+    }
+
 }
